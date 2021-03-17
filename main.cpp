dc479eb4f4c22dd259c163b876d548a997c9832f
zembrani <renato.zembrani@hotmail.com>
2021-03-16 22:52:48 -0300

    Temporizador

--- main.cpp
+++ main.cpp
@@ -1,8 +1,16 @@
 #include "lib/lib.hpp"
+#include <sys/time.h>
+
+double wtime() {
+  struct timeval t;
+  gettimeofday(&t, NULL);
+  return t.tv_sec + (double) t.tv_usec / 1000000;
+}
 
 int main(int argc, char const* argv[]) {
   FileOperator file;
   GetParameters parameters;
+  double initialTime, finalTime;
 
   int population = parameters.getPopulation();
   int interactions = parameters.getInteractions();
@@ -16,7 +24,10 @@ int main(int argc, char const* argv[]) {
   parameters.print(qtNos);
 
   Genetic genetic;
+  initialTime = wtime();
   genetic.run(data, population, interactions, qtNos, mutationDegree, survival);
+  finalTime = wtime();
+  cout << "Time: " << finalTime - initialTime << endl;
 
   return 0;
 }
